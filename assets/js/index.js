b0f20fa078fc590c171c44ec0a7a3dc8162cd68e
irorochad <wisdomchadere@gmail.com>
2022-02-13 03:00:27 +0100

    fixed some minor issues

--- assets/js/index.js
+++ assets/js/index.js
@@ -102,3 +102,21 @@ function initParadoxWay() {
 $(document).ready(function () {
   initParadoxWay();
 });
+
+
+
+
+
+$(document).ready(function(){
+  $("#testimonial-slider").owlCarousel({
+      items:1,
+      itemsDesktop:[1000,1],
+      itemsDesktopSmall:[979,1],
+      itemsTablet:[768,1],
+      pagination:true,
+      navigation:true,
+      navigationText:["",""],
+      slideSpeed:1000,
+      autoPlay:true
+  });
+})
