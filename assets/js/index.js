962d60e7dd68f62d6aed11582144dfef8ef31e32
irorochad <wisdomchadere@gmail.com>
2022-02-14 02:35:09 +0100

    added the learn more video sectiona nd still working on it.

--- assets/js/index.js
+++ assets/js/index.js
@@ -106,17 +106,5 @@ $(document).ready(function () {
 
 
 
+// when you hover the learn more video, this happens
 
-$(document).ready(function(){
-  $("#testimonial-slider").owlCarousel({
-      items:1,
-      itemsDesktop:[1000,1],
-      itemsDesktopSmall:[979,1],
-      itemsTablet:[768,1],
-      pagination:true,
-      navigation:true,
-      navigationText:["",""],
-      slideSpeed:1000,
-      autoPlay:true
-  });
-})
