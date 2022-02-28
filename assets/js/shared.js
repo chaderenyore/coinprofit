fcfd51f5b4acf0bed8223061b0b08dc22065119a
irorochad <wisdomchadere@gmail.com>
2022-02-28 01:33:57 +0100

    working on the leaders page

--- assets/js/shared.js
+++ assets/js/shared.js
@@ -83,3 +83,37 @@ function closebackdrop() {
 
 
 // End Navabr
+
+
+
+
+
+
+// Footer section
+// The footer section differs from each screen. Mobile and tablets are same while computers is different
+let toggles = document.getElementsByClassName('footerToggle');
+let contentBox = document.getElementsByClassName('footerContent');
+let icons = document.getElementsByClassName('footerIcon');
+
+for(let i=0; i<toggles.length; i++){
+    toggles[i].addEventListener('click', ()=>{
+        if( parseInt(contentBox[i].style.height) != contentBox[i].scrollHeight){
+            contentBox[i].style.height = contentBox[i].scrollHeight + "px";
+            icons[i].classList.remove('fa-chevron-down');
+            icons[i].classList.add('fa-chevron-up');
+        }
+        else{
+            contentBox[i].style.height = "0px";
+            icons[i].classList.remove('fa-chevron-up');
+            icons[i].classList.add('fa-chevron-down');
+        }
+
+        for(let j=0; j<contentBox.length; j++){
+            if(j!==i){
+                contentBox[j].style.height = "0px";
+                icons[j].classList.remove('fa-chevron-up');
+                icons[j].classList.add('fa-chevron-down');
+            }
+        }
+    });
+}
