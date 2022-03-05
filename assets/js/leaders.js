c3f8cc665cc938e664590c93477c1188fa5869eb
irorochad <wisdomchadere@gmail.com>
2022-03-05 14:15:31 +0100

    leaders page is done

--- assets/js/leaders.js
+++ assets/js/leaders.js
@@ -11,3 +11,32 @@ document.addEventListener('DOMContentLoaded', function () {
   })
   
   // end of splider js
+
+
+  // FAQ SECTION
+let faqtoggles = document.getElementsByClassName('faqtoggle');
+let faqcontentDiv = document.getElementsByClassName('faqcontent');
+let faqicons = document.getElementsByClassName('faqicon');
+
+for(let i=0; i<faqtoggles.length; i++){
+  faqtoggles[i].addEventListener('click', ()=>{
+        if( parseInt(faqcontentDiv[i].style.height) != faqcontentDiv[i].scrollHeight){
+          faqcontentDiv[i].style.height = faqcontentDiv[i].scrollHeight + "px";
+          faqicons[i].classList.remove('fa-chevron-down');
+          faqicons[i].classList.add('fa-chevron-up');
+        }
+        else{
+          faqcontentDiv[i].style.height = "0px";
+          faqicons[i].classList.remove('fa-chevron-up');
+          faqicons[i].classList.add('fa-chevron-down');
+        }
+
+        for(let j=0; j<faqcontentDiv.length; j++){
+            if(j!==i){
+            faqcontentDiv[j].style.height = "0px";
+            faqicons[j].classList.remove('fa-chevron-up');
+            faqicons[j].classList.add('fa-chevron-down');
+            }
+        }
+    });
+}
