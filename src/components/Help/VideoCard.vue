c1e4d752da992853dff537594ac404ea2f881494
chaderenyore <chaderenyore480@gmail.com>
2022-03-22 23:09:36 +0100

    Add first design of card list

--- src/components/Help/VideoCard.vue
+++ src/components/Help/VideoCard.vue
@@ -1,3 +1,67 @@
 <template>
-  <p>Just Some Video Card</p>
+  <ul v-if="helpvid" class="flex flex-col justify-center items-center">
+    <li v-for="video in helpvid.results" :key="video.uid">
+      <figure
+        class="video-list flex flex-row gap-3 items-center rounded-xl pr-8"
+      >
+        <div class="video-thumbnail h-28 w-32 rounded-xl">
+          <PrismicImage
+            :field="video.data.thumbnail"
+            class="w-[100%] h-[100%] rounded-xl"
+          />
+        </div>
+        <figcaption class="ml-3 flex flex-col">
+          <h1 class="text-2xl font-bold text-[#3374EA] mb-3">
+            {{ $prismic.asText(video.data.name) }}
+          </h1>
+          <p class="text-xl text-[#8993a7]">
+            {{ $prismic.asText(video.data.duration) }}
+          </p>
+        </figcaption>
+      </figure>
+    </li>
+  </ul>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        helpvid: null,
+      };
+    },
+    methods: {
+      async getData() {
+        this.helpvid = await this.$prismic.client.getByType("tutorial");
+        console.log(this.helpvid);
+      },
+    },
+    mounted() {
+      this.getData();
+    },
+  };
+</script>
+
+<style scoped>
+  .video-list {
+    background: var(--nav-light);
+    /* max-width: 29.625rem; */
+  }
+
+  .video-thumbnail {
+    display: inline-block;
+    background: linear-gradient(
+      180deg,
+      rgba(117, 231, 255, 1),
+      rgba(103, 123, 255, 1)
+    );
+    opacity: 0.8;
+  }
+
+  .video-thumbnail img {
+    position: relative;
+    z-index: -1;
+    display: block;
+    opacity: 0.2;
+  }
+</style>
