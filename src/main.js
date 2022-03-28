a9ee684b0b1a3582f1e541f09748e398a4520374
chaderenyore <chaderenyore480@gmail.com>
2022-03-28 01:49:08 +0100

    add plyr package and integrate it to the video player and added responsiveness

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import prismic from "./prismic/prismic.js";
 import "./tailwind/index.css";
+import VuePlyr from "vue-plyr";
+import "vue-plyr/dist/vue-plyr.css";
 const app = createApp(App);
 import BaseCallToAction from "@/components/BaseCallToAction.vue";
 
 app.component("BaseCallToAction", BaseCallToAction);
 
-app.use(router).use(prismic).mount("#app");
+app.use(router).use(prismic).use(VuePlyr).mount("#app");
