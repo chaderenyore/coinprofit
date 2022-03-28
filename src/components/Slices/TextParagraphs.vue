633ad4ae2eeab2ba382e7f1ec403c107ab8e5054
chaderenyore <chaderenyore480@gmail.com>
2022-03-28 19:33:01 +0100

    use v-tml to render the text coming from Prismic so that texts can be bold and links can be added

--- src/components/Slices/TextParagraphs.vue
+++ src/components/Slices/TextParagraphs.vue
@@ -1,12 +1,11 @@
 <template>
   <div>
-    <PrismicText
-      v-for="paragraph in slice.items"
-      :key="paragraph.length"
-      class="article-introtext text-base mb-6 font-medium"
-      wrapper="p"
-      :field="paragraph.paragraphs"
-    />
+    <div v-for="paragraph in slice.items" :key="paragraph.length">
+      <p
+        v-html="$prismic.asHTML(paragraph.paragraphs)"
+        class="article-introtext text-base mb-6 font-medium"
+      ></p>
+    </div>
   </div>
 </template>
 
@@ -20,7 +19,7 @@
 </script>
 
 <style scoped>
-  .article-introtext {
+  .article-introtext >>> p {
     color: var(--article-text);
   }
 </style>
