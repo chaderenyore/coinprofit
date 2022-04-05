337d21ed54ba41ab651b0fa66e6e55a88d8a8185
chaderenyore <chaderenyore480@gmail.com>
2022-04-05 01:39:23 +0100

    Add Lang to Html

--- src/views/HelpPage.vue
+++ src/views/HelpPage.vue
@@ -142,7 +142,11 @@
       },
     },
     mounted() {
-      if ("purpose" in this.$route.query) {
+      if (
+        "purpose" in this.$route.query &&
+        this.$route.query.purpose === "contact"
+      ) {
+        console.log(this.$route.query.purpose);
         const contactSection = document.querySelector("#contact-section");
         contactSection.scrollIntoView({ behavior: "smooth" });
       }
