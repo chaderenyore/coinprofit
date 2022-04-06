438ab8cb384c94df44510441f8494877ccf906db
chaderenyore <chaderenyore480@gmail.com>
2022-04-07 00:20:50 +0100

    Make Details Of Notfound component to be flexible

--- src/components/Help/NotFound.vue
+++ src/components/Help/NotFound.vue
@@ -2,24 +2,24 @@
   <div class="w-10/12 md:w-6/12 lg:w-5/12 m-auto rounded-lg p-5 shadow-lg">
     <h3 class="text-[#3374EA] mb-2">{{ errorMessage }}</h3>
     <p>
-      This Means that the article you're looking for does not exist or may have
+      This Means that the Document you're looking for does not exist or may have
       been moved.
     </p>
   </div>
 </template>
 
 <script>
-export default {
-  props: ["errorMessage"],
-};
+  export default {
+    props: ["errorMessage"],
+  };
 </script>
 
 <style scoped>
-div {
-  background-color: var(--article-container);
-}
+  div {
+    background-color: var(--article-container);
+  }
 
-p {
-  color: var(--help-article-text);
-}
+  p {
+    color: var(--help-article-text);
+  }
 </style>
