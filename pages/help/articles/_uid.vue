fb4797f919e1572536f400e3ef88f946b1698652
chaderenyore <chaderenyore480@gmail.com>
2022-05-05 17:43:35 +0100

    Add Uid text To refernce how to use open graph

--- pages/help/articles/_uid.vue
+++ pages/help/articles/_uid.vue
@@ -1,3 +1,13 @@
 <template>
-  <h1>A single Help article Post from the uid: {{ $route.params.uid }}</h1>
+  <div class="pt-48">
+    <h1>A single Help article Post from the uid: {{ $route.params.uid }}</h1>
+    <p>
+      Add Twitter Cards to all the Blog UID pages. Link to tutorial On how to do
+      it is found in
+      <a
+        href="https://www.everywheremarketer.com/blog/ultimate-guide-to-social-meta-tags-open-graph-and-twitter-cards#:~:text=FREE%20Cheatsheet%20Now-,Twitter%20Cards,you%20more%20options%20and%20control."
+        >Everywere Marketer</a
+      >
+    </p>
+  </div>
 </template>
